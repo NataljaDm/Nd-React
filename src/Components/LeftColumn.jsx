cf7cdc32e523005409588349a39f6b1890d3d976
Natalja <tasia@inbox.lt>
2023-07-28 13:36:13 +0300

    1 commit

---
+++ src/Components/LeftColumn.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "../App.css";
+
+
+const Column = () => {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <span>Dashboard</span>
+                </li>
+                <li>
+                    <span>Product</span>
+                    <div className="amount">2</div>
+                </li>
+                <li>
+                    <span>Store</span>
+                </li>
+                <li>
+                    <span>Visitor</span>
+                </li>
+                <li>
+                    <span>Analytics</span>
+                </li>
+                <li>
+                    <span>Notification</span>
+                    <div className="amount">11</div>
+                </li>
+                <li>
+                    <span>Help Center</span>
+                </li>
+                <li>
+                    <span>Settings</span>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Column;
