af509521082583dc8d8bde3b19cdcf4556d4c488
Natalja <tasia@inbox.lt>
2023-07-30 21:25:39 +0300

    commit

--- src/Components/Body/body.jsx
+++ src/Components/Body/body.jsx
@@ -5,21 +5,19 @@ import logo18 from '../Logo18.PNG';
 const Body = () => {
     return (
         <>
-            <div className="container">
-                <div className="list">
-                    <div className="container">
-                        <div className="grade-line">
-                            <div className="grade">
-                                <p clasName="grade-info">4.5</p>
-                            </div>
-                            <h3>Net Profit</h3>
+            <div className="list">
+                <div className="container">
+                    <div className="grade-line">
+                        <div className="grade">
+                            <p clasName="grade-info">4.5</p>
                         </div>
-                        <h2 class="number">302.1K</h2>
-                        <p class="text">29%vs$300.3k last year</p>
+                        <h3>Net Profit</h3>
                     </div>
+                    <h2 class="number">302.1K</h2>
+                    <h5 class="text">29%vs$300.3k last year</h5>
                 </div>
             </div>
-            <div className = "icon"><img src={logo18} alt="Logo18" /></div>
+            <div className="icon"><img src={logo18} alt="Logo18" /></div>
         </>
     )
 }
