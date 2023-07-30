c86d12a84e55ed7fabf687a32e5badc4546b1bf1
Natalja <tasia@inbox.lt>
2023-07-30 20:52:09 +0300

    modify logo

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Header from "./Components/Header/header";
 import Column from "./Components/LeftColumn";
 import Body from "./Components/Body/body";
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
+        
         <Column />
         <Body />
       </header>
--- src/Components/LeftColumn.jsx
+++ src/Components/LeftColumn.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import "../App.css";
-import logo from './Logo/Logo.PNG'
+import logo from '../Logo.PNG';
+import logo1 from '../Logo1.png';
+import logo14 from '../Logo14.png';
+import logo4 from '../Logo4.png';
+import logo5 from '../Logo5.png';
+import logo6 from '../Logo6.png';
+import logo7 from '../Logo7.png';
+import logo8 from '../Logo8.png';
+import logo9 from '../Logo9.png';
+import logo16 from '../Logo16.png';
 
 const Column = () => {
     return (
@@ -9,35 +18,44 @@ const Column = () => {
                 <div>
                     <img src={logo} alt="Logo" />
                 </div>
-                <li >
+                <div>
+                    <img className="pic" src={logo1} alt="Logo1" />
                     <span>Dashboard</span>
-                </li>
-                <li>
+                </div>
+                <div>
+                    <img className="pic" src={logo14} alt="Logo14" />
                     <span>Product</span>
                     <div className="amount">2</div>
-                </li>
-                <li>
+                </div>
+                <div>
+                    <img className="pic" src={logo4} alt="Logo4" />
                     <span>Store</span>
-                </li>
-                <li>
+                </div>
+                <div>
+                    <img className="pic" src={logo5} alt="Logo5" />
                     <span>Visitor</span>
-                </li>
-                <li>
+                </div>
+                <div>
+                    <img className="pic" src={logo6} alt="Logo6" />
                     <span>Analytics</span>
-                </li>
-                <li>
+                </div>
+                <div>
+                    <img className="pic" src={logo7} alt="Logo7" />
                     <span>Notification</span>
                     <div className="amount">11</div>
-                </li>
-                <li>
+                </div>
+                <div>
+                    <img className="pic" src={logo8} alt="Logo8" />
                     <span>Help Center</span>
-                </li>
-                <li>
+                </div>
+                <div>
+                    <img className="pic" src={logo9} alt="Logo9" />
                     <span>Settings</span>
-                </li>
-                <li>
+                </div>
+                <div>
+                    <img className="pic" src={logo16} alt="Logo16" />
                     <span>Dark Mode</span>
-                </li>
+                </div>
             </ul>
         </div>
     )
